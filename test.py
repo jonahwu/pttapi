43f977213aebfe59ef29fa307828fb95164ca1f2
jonah wu <jonah.wu@crowdinsight.com.tw>
2021-07-27 11:46:32 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,16 @@
+
+import time
+
+
+def test11():
+   for i in range(10):
+        print(f'test{i}')
+        #time.sleep(2)
+
+
+test11()
+print('test')
+for i in range(100):
+    print(f'it is about {i}')
+    time.sleep(1)
+
